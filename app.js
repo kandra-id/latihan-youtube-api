cc4846d59ff9cf56a6230d20d0a853f83e210aab
kandra-id <40682581+kandra-id@users.noreply.github.com>
2021-12-09 22:18:38 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+$.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/channels',
+    method: 'GET',
+    dataType: 'json',
+    data: {
+        part: 'snippet,contentDetails,statistics',
+        id: 'UCaL1L-np8fBWIW4jPE7IwcQ', // Youtube Channel ID
+        key: 'AIzaSyC8YWqhiubsu_R6xEOpUM-dZQ7b6D4ihaM' // Google Console API KEY. Warning!!! Get Your Own API KEY.
+    },
+    success: function (data) {
+        let profileImgUrl = data.items[0].snippet.thumbnails.default.url;
+        $("img").attr({
+            src: profileImgUrl,
+            alt: 'Kandra Wilko'
+        });
+    }
+});
+
